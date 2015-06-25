ea4ebf64c6642dd9526e0877f195310770a351e2
Zalewa <zalewapl@gmail.com>
2015-06-25 11:46:07 +0200

    Main page says "Hi!" now.

--- doomstats/settings.py
+++ doomstats/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stats'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -123,4 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
--- stats/views.py
+++ stats/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def front_page(request):
+    return render(request, "stats/front_page.html")
