7d1e13915859af754613eefaa0b52aa2a81db29e
Zalewa <zalewapl@gmail.com>
2015-06-26 18:16:19 +0200

    Display basic statistics on the front page.

--- stats/views.py
+++ stats/views.py
@@ -3,7 +3,12 @@ from .models import *
 
 
 def front_page(request):
-    return render(request, "stats/front_page.html")
+    data = {}
+    data["first_batch"] = RefreshBatch.objects.order_by("date")[0]
+    data["num_batches"] = RefreshBatch.objects.count()
+    data["average_server_count"] = Server.objects.count() / data["num_batches"]
+    data["average_player_count"] = Player.objects.count() / data["num_batches"]
+    return render(request, "stats/front_page.html", data)
 
 
 def engine(request, name):
