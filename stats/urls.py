0b076ed6100867440490fde6388d2b141ebbde36
Zalewa <zalewapl@gmail.com>
2015-06-26 14:48:46 +0200

    Very basic page layout.

--- stats/urls.py
+++ stats/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.front_page)
+    url(r'^$', views.front_page, name='front_page'),
+    url(r'^engine/(?P<name>[ a-zA-Z]+)/$', views.engine, name='engine')
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 
 def front_page(request):
     return render(request, "stats/front_page.html")
+
+
+def engine(request, name):
+    return render(request, "stats/engine.html", {"name": name})
+
+
+def load_site_global_context(request):
+    return {
+        "engines": Engine.objects.all().order_by("name")
+    }
