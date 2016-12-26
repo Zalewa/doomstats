b1a562ea2105e4992fa51d7ba49a99c1955b01b3
Zalewa <zalewapl@gmail.com>
2016-12-26 23:35:44 +0100

    Support game engines with numbers in name.

--- stats/urls.py
+++ stats/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.front_page, name='front_page'),
-    url(r'^engine/(?P<name>[ a-zA-Z]+)/$', views.engine_players, name='engine'),
-    url(r'^engine/(?P<name>[ a-zA-Z]+)/wads/$', views.engine_wads, name='wads'),
-    url(r'^engine/(?P<name>[ a-zA-Z]+)/servers/$', views.engine_servers, name='servers'),
+    url(r'^engine/(?P<name>[ a-zA-Z0-9]+)/$', views.engine_players, name='engine'),
+    url(r'^engine/(?P<name>[ a-zA-Z0-9]+)/wads/$', views.engine_wads, name='wads'),
+    url(r'^engine/(?P<name>[ a-zA-Z0-9]+)/servers/$', views.engine_servers, name='servers'),
     url(r'^about/$', views.about, name='about')
 ]
