a63153082a74afd10837f9accd6c4e7b140896e3
Zalewa <zalewapl@gmail.com>
2016-12-29 14:47:19 +0100

    Build script updated - remove .pyc files from build.

--- build-deployment-package.sh
+++ build-deployment-package.sh
@@ -2,27 +2,35 @@
 #
 # They told me about using automatic deployment tools
 # such as 'fabric'... BUT I DIDN'T LISTEN!
+#
 
+# Preparatory steps. Spawn new directory in temp space.
 set -e
 
 scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 workdir=`pwd`
-tmpname=`uuidgen`
-builddir=/tmp/$tmpname/doomstats
+builddir=`mktemp -d`/doomstats
 
-mkdir -p $builddir
-cp -R $scriptdir/* $builddir
-cp -R $scriptdir/.git $builddir
-
-cd $builddir
-git clean -fd
+# Go to the directory where this script is located and get
+# the commit hash of the current HEAD.
+cd $scriptdir
 githash=`git log -n 1 --format=format:%h`
-rm -rf $builddir/.git
+
+# Export current HEAD to the build directory.
+mkdir -p $builddir
+git worktree add "$builddir" HEAD
+
+# Go to the build directory and build.
+cd $builddir
+rm .git
+rm .gitignore
 python ./manage.py collectstatic --noinput
 sed -i -r 's/^DEBUG\s*=.*True$/DEBUG = False/' $builddir/doomstats/settings.py
 sed -i -r 's/^ALLOWED_HOSTS\s*=.*$/ALLOWED_HOSTS = ["*"]/' $builddir/doomstats/settings.py
+pyclean -v .
 cd ..
 
+# Package the build.
 stamp=`date +%Y%m%d-%H%M%S`
 tar -cvzf "$workdir/doomstats-$stamp-$githash.tar.gz" ./doomstats
 
